f72d15ab860035dfcdf4094ff65b85a15e3db868
Damian Fiłonowicz <damian.filonowicz@gmail.com>
2022-06-01 14:52:37 +0200

    Add the first version of the demo

---
+++ terraform/orkuz-2022L.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.7.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "orkuz-rg" {
+  name     = "orkuz-rg"
+  location = "eastasia"
+}
+
+resource "azurerm_kubernetes_cluster" "aks-orkuz" {
+  name                = "aks-orkuz"
+  location            = azurerm_resource_group.orkuz-rg.location
+  resource_group_name = azurerm_resource_group.orkuz-rg.name
+  dns_prefix          = "orkuz-2022L"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+
+    tags = {
+      pool = "default-wtorek"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "additional" {
+  name                  = "additional"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-orkuz.id
+  vm_size               = "Standard_F2s_v2"
+  node_count            = 1
+
+  tags = {
+    pool = "additional-wtorek"
+  }
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "az aks get-credentials --resource-group ${azurerm_resource_group.orkuz-rg.name} --name ${azurerm_kubernetes_cluster.aks-orkuz.name} --overwrite-existing"
+  }
+}
